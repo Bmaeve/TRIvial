4e98143bef8a82a698f3c9981761f8c77053eec2
jonathanboris <ouedraogojonathanboris@gmail.com>
2023-03-29 21:04:49 +0200

    secours front

--- front/src/components/Secours/Section.vue
+++ front/src/components/Secours/Section.vue
@@ -15,29 +15,34 @@
             </div>
 
         </div>
-        <br>
-        <div class="sec_info_enjeux">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Properties</th>
-                        <th scope="col">value</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <template v-for="(properties, key) in getfeatureKeys" :key='key'>
-                        <tr>
-                            <th>{{ properties }}</th>
-                            <td>{{ getfeatureinfo[0][properties] }}</td>
-                        </tr>
 
-                    </template>
-                </tbody>
-            </table>
+        <div class="sec_info_enjeux">
+            <span>Informations</span>
+            <div class="sec_info_enjeux_table">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Properties</th>
+                            <th scope="col">value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <template v-for="(properties, key) in getfeatureKeys" :key='key'>
+                            <tr>
+                                <th>{{ properties }}</th>
+                                <td>{{ getfeatureinfo[0][properties] }}</td>
+                            </tr>
+
+                        </template>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
 
         <div class="sec_calcul_itin">
-            <div>
+            <span>Itineraire</span>
+            <div class="sec_itin">
                 <button type="button" class="btn btn-secondary rounded float-start">
                     <img src="../../assets/start.png" width="35" height="35" />
                     <span>Start</span>
@@ -55,7 +60,7 @@
         </div>
         <br>
         <div class="sec_secour_logo">
-            <img src="../../assets/logo.png" width="60" height="60" />
+            <img src="../../assets/logo.png" width="50" height="50" />
         </div>
     </div>
 </template>
@@ -96,6 +101,13 @@ export default {
             {
                 "id": 3,
                 "title": "Plan 3",
+                "Script": {
+                    "sql": "select * from A"
+                }
+            },
+            {
+                "id": 4,
+                "title": "Plan 4",
                 "Script": {
                     "sql": "select * from A"
                 }
@@ -140,21 +152,34 @@ export default {
 }
 
 .sec_info_enjeux {
-    height: 35vh;
-    overflow-y: auto;
     color: white !important;
+
+}
+
+.sec_info_enjeux table {
     background-color: white;
 }
 
-.sec_calcul_itin {
-    height: 16vh;
+.sec_info_enjeux_table {
     overflow-y: auto;
+    height: 32vh;
+}
+
+.sec_calcul_itin {
+
+    overflow-y: auto;
+
+}
+
+.sec_itin {
     background-color: white;
     margin-top: 10px;
     padding: 10px;
+    height: 16vh;
 }
 
 .sec_secour_logo {
     text-align: center;
+    color: white;
 }
 </style>
