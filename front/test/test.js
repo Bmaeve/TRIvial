ca2e16043f14da7c8f6881dc9fa71d73ffd4a82c
baptisteRiviere <riviereb85@gmail.com>
2023-03-29 12:17:02 +0200

    init docker compose test & tests front

--- front/test/test.js
+++ front/test/test.js
@@ -14,15 +14,44 @@ describe('Array', function () {
 describe('API', function () {
     describe('#test', function () {
         it('should return a geojson array', function () {
-            fetch('http://localhost:3000/', {
+            let table_name = "bati_indiferrencie";
+            return fetch(host + 'data/' + table_name + '/selectData', {
                 method: 'get'
             })
-                .then(res => { res.json() })
+                //.then(res => res.json())
                 .then(data => {
                     console.log(data);
+                    assert.equal([1, 2, 3].indexOf(4), -1);
                 })
+                .catch(err => {
+                    console.log(err);
+                });
         })
-        assert.equal([1, 2, 3].indexOf(4), -1);
     });
 });
 
+/*
+// testing API
+describe('API', function () {
+    describe('#test', function () {
+        it('should return a geojson array', function () {
+            let table_name = "bati_indiferrencie";
+            let parameters = { "hauteur": { "min": 0, "max": 100 }, "origin_bat": { "values": ["Autre"] } }
+
+            return fetch(host + 'data/' + table_name + '/selectData', {
+                body: JSON.stringify(parameters),
+                headers: { 'Content-Type': 'application/json' },
+                method: 'post'
+            })
+                //.then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    assert.equal([1, 2, 3].indexOf(4), -1);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        })
+    });
+});
+*/
